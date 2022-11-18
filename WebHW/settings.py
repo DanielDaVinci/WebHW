adb3c021a341ba50c64e041cc60a39d94b8c6de1
Daniel da Vinci <daniil11072003@mail.ru>
2022-11-18 11:52:04 +0300

    HW3 without DataBase

--- WebHW/settings.py
+++ WebHW/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -64,9 +64,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-
     }
-
 }
 
 # Password validation
--- app/models.py
+++ app/models.py
@@ -1,42 +1,136 @@
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
-
-# Create your models here.
-
-
-class question:
-    def __init__(self, question_id, title, text, tags, answers):
-        self.id = question_id
-        self.title = title
-        self.text = text
-        self.tags = tags
-        self.answers = answers
-        pass
+from django.http import Http404
 
 
-def createAnswers(amount):
-    return [
-        {
-            'id': answer_id,
-            'text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' * answer_id,
-            'correct': True,
-        } for answer_id in range(amount)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(blank=True, null=True, upload_to="media")
+
+    def __str__(self):
+        return self.user.username
+
+class QuestionManager(models.Manager):
+    def get_hot_questions(self):
+        return self.filter(status__exact='h')
+
+    def get_new_questions(self):
+        return self.order_by('-date')
+
+    def get_tagged_questions(self, tag):
+        return self.filter(tag__exact=tag)
+
+    def get_questions(self):
+        return self.all()
+
+    def find_by_id(self, id):
+        try:
+            vote = self.get(pk=id)
+        except ObjectDoesNotExist:
+            raise Http404
+        return vote
+
+
+class Question(models.Model):
+    profile = models.ForeignKey(Profile, models.PROTECT)
+
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+
+    tags = None
+    answers_count = 0
+    like_number = 0
+
+    HOT = 'h'
+    NORMAL = 'n'
+    STATUSES = [
+        (HOT, 'hot'),
+        (NORMAL, 'normal')
     ]
+    status = models.CharField(max_length=1, choices=STATUSES)
+    date = models.DateTimeField(blank=True, null=True)
+
+    objects = QuestionManager()
+    def __str__(self):
+        return f'{self.title}'
 
 
-QUESTIONS = [
-    {
-        'id': question_id,
-        'title': f'Question #{question_id}',
-        'text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' * question_id,
-        'answers_number': question_id*question_id,
-        'tags': (f'tag #{i+1}' for i in range(question_id)),
-        'answers': createAnswers(question_id)
-    } for question_id in range(10)
-]
+class TagManager(models.Manager):
+
+    def get_tags_by_question(self, question):
+        return self.filter(question__exact=question)
+
+    def find_by_id(self, id):
+        try:
+            vote = self.get(pk=id)
+        except ObjectDoesNotExist:
+            raise Http404
+        return vote
+    def get_tags(self):
+        return self.all()
 
 
-TAGS = [
-    {
-        'name': "Ta"+"g"*tag_id
-    } for tag_id in range(10)
-]
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+    objects = TagManager()
+
+    question = models.ManyToManyField(Question)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class AnswerManager(models.Manager):
+    def get_answers(self, question):
+        return self.filter(question__exact=question)
+
+    def get_answers_count(self, question):
+        return self.filter(question__exact=question).count()
+
+
+class Answer(models.Model):
+    profile = models.ForeignKey(Profile, models.PROTECT)
+    question = models.ForeignKey(Question, models.CASCADE)
+
+    text = models.TextField()
+    correct = models.BooleanField(default=False)
+
+    like_number = 0
+
+    objects = AnswerManager()
+
+    def __str__(self):
+        return  self.text[:100]
+
+
+class LikeQuestionManager(models.Manager):
+    def get_questions_likes(self, question):
+        return self.filter(question__exact=question).count()
+
+
+class LikeQuestion(models.Model):
+    profile = models.ForeignKey(Profile, models.CASCADE)
+    question = models.ForeignKey(Question, models.CASCADE)
+
+    objects = LikeQuestionManager()
+
+
+class LikeAnswerManager(models.Manager):
+    def get_answers_likes(self, answer):
+        return self.filter(answer__exact=answer).count()
+
+
+class LikeAnswer(models.Model):
+    profile = models.ForeignKey(Profile, models.CASCADE)
+    answer = models.ForeignKey(Answer, models.CASCADE)
+
+    objects = LikeAnswerManager()
+
+
+OPTIONS = {
+    'username': 'Cat',
+    'password': 'password',
+    'nickname': 'Bird',
+    'is_auth': True
+}
